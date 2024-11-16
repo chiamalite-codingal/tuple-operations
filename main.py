66ce649a91ab1c0fd89bb2b582819ae4d0e75787
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-11-16 14:23:16 +0100

    tuple

---
+++ main.py
@@ -0,0 +1,16 @@
+#1
+x = ("Chiamalite",5,True,4.78)
+print(x)
+#2
+x = (3,4,5,6,7)
+print(x)
+#3
+x = x + (7,)
+print(x)
+#4
+X = (20,40,40,30)
+print(x.count(40))
+#5
+x = (3,4,5,6,7,8)
+slice = x[3:6]
+print(slice)
